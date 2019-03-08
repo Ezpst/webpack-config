9e3130c7e95878903b3ca132ca9aeb801d02e8c1
PanST <pansitong@shellunion.com>
2019-03-08 17:55:06 +0800

    commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,18 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin'); // 引入CleanWebpackPlugin插件
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');//压缩css插件
 
 module.exports = merge(common, {   // 将webpack.common.js合并到当前文件
-  devtool: 'source-map',  // 会生成对于调试的完整的.map文件，但同时也会减慢打包速度
+  devtool: '#source-map',  // 会生成对于调试的完整的.map文件，但同时也会减慢打包速度
   plugins: [
     new CleanWebpackPlugin(['dist']),  // 所要清理的文件夹名称
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: require('cssnano'), //引入cssnano配置压缩选项
+      cssProcessorOptions: { 
+        discardComments: { removeAll: true } 
+      },
+      canPrint: true //是否将插件信息打印到控制台
+    })
   ]
 })
